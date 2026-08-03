627abd065247529922ee313f8bc6e8cb2ebcae7a
shauryam2211-usm <shauryam2211@gmail.com>
2026-08-04 02:22:44 +0530

    Ignore node_modules

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express" ;
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+const app=express();
+app.post("api/vi/signup",(req,res)=>{
+
+})
+app.post("api/vi/signin",(req,res)=>{
+
+})
+app.post("api/vi/content",(req,res)=>{
+
+})
+app.get("api/vi/content",(req,res)=>{
+
+})
+app.delete("api/vi/content",(req,res)=>{
+
+})
+app.post("api/vi/brain/share",(req,res)=>{
+
+})
+app.get("api/vi/brain/shareLink",(req,res)=>{
+
+})
